38943089600e82d34311892538072e8a52aa5c9f
R <97416032@qq.com>
2023-04-01 22:38:28 +0800

    最终解

--- 33. 搜索旋转排序数组.cpp
+++ 33. 搜索旋转排序数组.cpp
@@ -2,29 +2,28 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int l=0,r=nums.size()-1,mid=(l+r)/2;
-        while(l<r){
+        if(r==0)
+            return nums[0]==target?0:-1;
+        while(l<=r){                                                                               
             if(nums[mid]==target)
                 return mid;
-            if(nums[l]>nums[r]){
-                if(nums[l]<=target&&target<nums[mid]||nums[mid]<nums[r]&&target>nums[r]){
-                r=mid-1;
-                mid=(l+r)/2;
-                }
-                else if(target<=nums[r]&&target>nums[mid]||target<nums[l]&&nums[mid]>nums[l]){
-                    l=mid+1;
-                    mid=(l+r)/2;
-                }
-            }
-            else{
-                if(nums[mid]>target){
+            if(nums[mid]>=nums[l]){
+                if(target<nums[mid]&&target>=nums[l]){
                     r=mid-1;
-                    mid=(l+r)/2;
                 }
                 else{
                     l=mid+1;
-                    mid=(l+r)/2;
                 }
             }
+            else{
+                if(target>nums[mid]&&target<=nums[r]){
+                    l=mid+1;
+                }
+                else{
+                    r=mid-1;
+                }
+            }
+            mid=(l+r)/2;
         }
         return -1;
     }
